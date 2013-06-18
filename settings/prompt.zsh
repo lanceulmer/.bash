01fb8af928e5b6a640d6e1fb0df69e6ada6a1a5b
Lance Ulmer <lance@lanceulmer.com>
2013-06-18 13:29:45 -0700

    Ported my old favorite theme from oh-my-zsh

--- settings/prompt.zsh
+++ settings/prompt.zsh
@@ -1,15 +1,27 @@
-function git_prompt_info {
-  local ref=$(git symbolic-ref HEAD 2> /dev/null)
-  local gitst="$(git status 2> /dev/null)"
+# a port of the robbyrussell theme from oh-my-zsh
+GIT_PROMPT_PREFIX="git:(%{$fg[red]%}"
+GIT_PROMPT_SUFFIX="%{$reset_color%}"
+GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗%{$reset_color%}"
+GIT_PROMPT_CLEAN="%{$fg[blue]%})"
 
-  if [[ ${gitst} =~ "use \"git add" ]]; then
-    gitstatus="%{$fg[red]%}!%{$reset_color%}"
+# get the name of the branch we are on
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo "$GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$GIT_PROMPT_SUFFIX"
+}
+
+# Checks if working tree is dirty
+parse_git_dirty() {
+  local GIT_STATUS=''
+  local CLEAN_MESSAGE='nothing to commit (working directory clean)'
+
+  GIT_STATUS=$(git status -s 2> /dev/null | tail -n1)
+
+  if [[ -n $GIT_STATUS ]]; then
+    echo "$GIT_PROMPT_DIRTY"
   else
-    gitstatus=''
-  fi
-
-  if [[ -n $ref ]]; then
-    echo "%{$fg_bold[green]%}(${ref#refs/heads/})%{$reset_color%}$gitstatus"
+    echo "$GIT_PROMPT_CLEAN"
   fi
 }
 
