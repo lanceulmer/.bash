d32f6cbf7d37d5ef418021470791d6d2a7ca21a3
Lance Ulmer <lance@lanceulmer.com>
2014-04-10 13:15:07 -0700

    Removed .gemrc

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ ln -fs ~/.zsh/.zshrc ~/.zshrc
 # other dotfiles
 ln -fs ~/.zsh/.gitconfig ~/.gitconfig
 ln -fs ~/.zsh/.gitignore_global ~/.gitignore_global
-ln -fs ~/.zsh/.gemrc ~/.gemrc
 ln -fs ~/.zsh/.ackrc ~/.ackrc
