5052d5526be2271d0dfb658c749749e619534f2e
Lance Ulmer <lance@lanceulmer.com>
2019-04-06 09:26:10 -0700

    add vscode settings

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # main bash file
 ln -fs ~/.bash/.bash_profile ~/.bash_profile
 
+# vscode settings / keybindings
+ln -fs ~/.bash/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -fs ~/.bash/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
 # other dotfiles
 ln -fs ~/.bash/.gitconfig ~/.gitconfig
 ln -fs ~/.bash/.gitignore_global ~/.gitignore_global
