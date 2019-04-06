4ead0578569d15479545fa85c779a63036e6bec5
Lance Ulmer <lance@lanceulmer.com>
2019-04-06 09:31:50 -0700

    install vscode settings last

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 # main bash file
 ln -fs ~/.bash/.bash_profile ~/.bash_profile
 
-# vscode settings / keybindings
-ln -fs ~/.bash/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -fs ~/.bash/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-
 # other dotfiles
 ln -fs ~/.bash/.gitconfig ~/.gitconfig
 ln -fs ~/.bash/.gitignore_global ~/.gitignore_global
 ln -fs ~/.bash/.gemrc ~/.gemrc
+
+# vscode settings / keybindings
+ln -fs ~/.bash/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -fs ~/.bash/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
